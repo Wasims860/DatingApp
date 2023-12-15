bda86a8fa20ccbea7b357f1f9e5c6b0a463b0da7
wasim shehadeh <shehadeh.w.sssd@gmail.com>
2023-12-15 20:58:10 +0300

    end section 4

---
+++ API/Extenstions/ApplicationServiceExtensions.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extenstions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+        {
+                services.AddDbContext<DataContext>(option=>
+                    {
+                    option.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                    });
+                    services.AddCors();
+                    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                    services.AddEndpointsApiExplorer();
+
+                    services.AddScoped<ITokenService,TokenService>();
+                    return services;
+
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,34 +1,36 @@
+using System.Text;
 using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extenstions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(option=>
-{
-option.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+//if (app.Environment.IsDevelopment())
+//{
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
